71274e841270f26de44799af2947276a9b9869e3
Matthew Singh <mattsingh@hotmail.com>
2022-04-05 18:10:09 -0400

    Complete login endpoint

--- api.js
+++ api.js
@@ -1,3 +1,9 @@
+const {
+	generateAccessToken,
+	decodeAccessToken,
+	authenticateToken,
+} = require('./jwt');
+
 exports.setApp = function (app, db) {
 	app.post('/auth/register', async (req, res) => {
 		const { firstName, lastName, email, password, confirmPassword } =
@@ -42,5 +48,36 @@ exports.setApp = function (app, db) {
 			[firstName + lastName, email, password, universityId]
 		);
 		res.status(200).json({ message: 'User created' });
+	});
+
+	app.post('/auth/login', async (req, res) => {
+		const { email, password } = req.body;
+
+		// Check if all fields are filled
+		if (!email || !password) {
+			return res.status(400).json({ message: 'All fields are required' });
+		}
+
+		// Check if user exists
+		const user = await db.query('SELECT * FROM users WHERE email = $1', [
+			email,
+		]);
+		if (user.rows.length === 0) {
+			return res.status(400).json({ message: 'User does not exist' });
+		}
+
+		// Check if password is correct
+		if (user.rows[0].password !== password) {
+			return res.status(400).json({ message: 'Incorrect password' });
+		}
+
+		// Create token
+		const token = generateAccessToken(
+			user.rows[0].id,
+			user.rows[0].university_id,
+			user.rows[0].auth_level
+		);
+
+		res.status(200).json({ token });
 	});
 };

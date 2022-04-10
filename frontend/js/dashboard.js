127eae1a50b34405e605ca39e42bd1dae918fdfa
Exdol <exdoldavy@Knights.ucf.edu>
2022-04-09 20:32:54 -0400

    Addition of dashboard components

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,14 +1,5 @@
 const origin = window.location.origin;
 
-// Redirect the user to the home page if there isn't a valid login token
-if (localStorage.getItem('token') == null) {
-
-    console.log("Redirecting back to home page due to lack of valid credentials");
-    // Redirect to home page
-    window.location.href = origin + '/';
-}
-
-
 // Logout
 const logout = function() {
     // Delete token in local storage

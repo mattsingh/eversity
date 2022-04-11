2ccefd18123a9ef54a752784e1c92b2e9c096bf8
Exdol <exdoldavy@Knights.ucf.edu>
2022-04-11 14:02:36 -0400

    Addition of events page and appropriate URL

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const db = new Client({
-	connectionString: process.env.DATABASE_URL,
-	ssl: {
-		rejectUnauthorized: false,
-	},
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 db.connect();
@@ -21,24 +21,28 @@ const port = process.env.PORT || 8080;
 app.use(express.static('frontend'));
 
 app.use(
-	'/js',
-	express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js'))
+    '/js',
+    express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js'))
 );
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'frontend/pages/index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/pages/index.html'));
 });
 
-app.get('/login', function (req, res) {
-	res.sendFile(path.join(__dirname, 'frontend/pages/login.html'));
+app.get('/login', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/pages/login.html'));
 });
 
-app.get('/register', function (req, res) {
-	res.sendFile(path.join(__dirname, 'frontend/pages/register.html'));
+app.get('/register', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/pages/register.html'));
 });
 
-app.get('/dashboard', function (req, res) {
+app.get('/dashboard', function(req, res) {
     res.sendFile(path.join(__dirname, 'frontend/pages/dashboard.html'));
+});
+
+app.get('/events', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/pages/events.html'));
 });
 
 app.use(bodyParser.json());

17aa590f2fd80a001f8e8e3a55f637f76b825d62
Exdol <exdoldavy@Knights.ucf.edu>
2022-04-13 13:14:30 -0400

    Pending events for super admins only

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -4,7 +4,7 @@ let recentEvents = [];
 let upcomingEvents = [];
 let jwt_contents = {};
 
-function parseJwt (token) {
+function parseJwt(token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
@@ -14,38 +14,38 @@ function parseJwt (token) {
     return JSON.parse(jsonPayload);
 };
 
-const setRecentEvents = function () {
-	events.forEach((event) => {
-		let eventDate = new Date(event.date);
-		if (eventDate < today) recentEvents.push(event);
-	});
-	console.log('Recent Events');
-	console.log(recentEvents);
+const setRecentEvents = function() {
+    events.forEach((event) => {
+        let eventDate = new Date(event.date);
+        if (eventDate < today) recentEvents.push(event);
+    });
+    console.log('Recent Events');
+    console.log(recentEvents);
 };
 
-const setUpcomingEvents = function () {
-	events.forEach((event) => {
-		let eventDate = new Date(event.date);
-		if (eventDate > today) upcomingEvents.push(event);
-	});
-	console.log('Upcoming Events');
-	console.log(upcomingEvents);
+const setUpcomingEvents = function() {
+    events.forEach((event) => {
+        let eventDate = new Date(event.date);
+        if (eventDate > today) upcomingEvents.push(event);
+    });
+    console.log('Upcoming Events');
+    console.log(upcomingEvents);
 };
 
 // Load Events
-const loadEvents = function () {
-	// Select upcoming events container
-	const upcomingEventsContainer = document.getElementById(
-		'upcomingEventsContainer'
-	);
-	// Select recent events container
-	const recentEventsContainer = document.getElementById(
-		'recentEventsContainer'
-	);
+const loadEvents = function() {
+    // Select upcoming events container
+    const upcomingEventsContainer = document.getElementById(
+        'upcomingEventsContainer'
+    );
+    // Select recent events container
+    const recentEventsContainer = document.getElementById(
+        'recentEventsContainer'
+    );
 
-	let htmlString = '';
-	upcomingEvents.forEach((event) => {
-		htmlString += `
+    let htmlString = '';
+    upcomingEvents.forEach((event) => {
+        htmlString += `
             <div class="card eventCell bg-secondary">
                 <img class="eventCellPicture" src="https://media.discordapp.net/attachments/958034245551534132/962500224394334249/unknown.png" alt="Event picture" />
                 <h3 class="card-title">${event.name}</h6>
@@ -53,13 +53,13 @@ const loadEvents = function () {
                 <p class="eventCellParagraph">${event.description}</p>
             </div>
         `;
-	});
+    });
 
-	upcomingEventsContainer.innerHTML = htmlString;
+    upcomingEventsContainer.innerHTML = htmlString;
 
-	htmlString = '';
-	recentEvents.forEach((event) => {
-		htmlString += `
+    htmlString = '';
+    recentEvents.forEach((event) => {
+        htmlString += `
         <div class="card eventCell bg-secondary">
             <img class="eventCellPicture" src="https://media.discordapp.net/attachments/958034245551534132/962500224394334249/unknown.png" alt="Event picture" />
             <h3 class="card-title">${event.name}</h6>
@@ -67,41 +67,76 @@ const loadEvents = function () {
             <p class="eventCellParagraph">${event.description}</p>
         </div>
         `;
-	});
+    });
 
-	recentEventsContainer.innerHTML = htmlString;
-	console.log('Rendered Events to DOM');
+    recentEventsContainer.innerHTML = htmlString;
+    console.log('Rendered Events to DOM');
 };
 
 // Get all events
-const getEvents = async function () {
-	// Load JWT from local storage
-	const token = localStorage.getItem('token');
-	await axios
-		.get(origin + '/event/get', {
-			headers: { Authorization: 'Bearer ' + token },
-		})
-		.then(function (res) {
-			console.log(res);
-			if (res.status === 200) {
-				events = res.data;
-			}
-		})
-		.catch(function (err) {
-			console.log(err);
-			alert(err.response.data.message);
-		});
+const getEvents = async function() {
+    // Load JWT from local storage
+    const token = localStorage.getItem('token');
+    await axios
+        .get(origin + '/event/get', {
+            headers: { Authorization: 'Bearer ' + token },
+        })
+        .then(function(res) {
+            console.log(res);
+            if (res.status === 200) {
+                events = res.data;
+            }
+        })
+        .catch(function(err) {
+            console.log(err);
+            alert(err.response.data.message);
+        });
 };
 
-// On page load get events
-document.addEventListener('DOMContentLoaded', async function () {
-    // TODO: Redirect to login if no token
-	await getEvents();
-	setRecentEvents();
-	setUpcomingEvents();
-	loadEvents();
+// Load all upcoming events that are not approved
+const loadPendingEvents = async function(auth_level) {
 
-	// Load JWT from local storage
-	const token = localStorage.getItem('token');
-	jwt_contents = parseJwt(token);
+    // Get the pending event container
+    const pendingEventsLocation = document.getElementById('pendingEvents');
+
+    if (auth_level == 2) {
+        alert("Welcome, Super Admin");
+        // Load pending events container
+        let htmlString = `<h1>Pending Events</h1>
+        <div id="pendingEventsContainer" class="card">`;
+
+        upcomingEvents.forEach((event) => {
+            if (event.approved == false) {
+                htmlString += `<div class="card eventCell bg-secondary">
+			<img class="eventCellPicture" src="https://media.discordapp.net/attachments/958034245551534132/962500224394334249/unknown.png" alt="Event picture" />
+			<h3 class="card-title">${event.name}</h6>
+			<h5 class="time">${new Date(event.date).toDateString()}</h5>
+				<p class="eventCellParagraph">${event.description}</p>
+				<button class="approveRSO" onclick="approveRSO()">Approve</button>
+				<button class="denyRSO" onclick="denyRSO()">Deny</button>
+		</div>
+		`;
+            }
+        });
+        // Close the container at the end
+        htmlString += '</div>';
+
+        pendingEventsLocation.innerHTML = htmlString;
+        console.log('Rendered Pending Events to DOM for Super Admin');
+    }
+
+}
+
+// On page load get events
+document.addEventListener('DOMContentLoaded', async function() {
+    // TODO: Redirect to login if no token
+    await getEvents();
+    setRecentEvents();
+    setUpcomingEvents();
+    loadEvents();
+
+    // Load JWT from local storage
+    const token = localStorage.getItem('token');
+    jwt_contents = parseJwt(token);
+    loadPendingEvents(jwt_contents.auth_level);
 });
